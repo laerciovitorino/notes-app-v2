35797b062853ce3186072657637105949ba7905f
Laércio Vitorino <laerciovitorino@gmail.com>
2026-09-02 12:52:44 -0300

    Add yargs package and implement commands add, remove, list and read first verions

--- app.js
+++ app.js
@@ -1,8 +1,35 @@
 import chalk from 'chalk';
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
 import { getNotes } from './notes.js';
 
-const msg = getNotes();
-console.log(msg);
-
-const successMsg = chalk.green.bold('Success!');
-console.log(successMsg);
+yargs()
+  .command(
+    'add',
+    'Add a new note',
+    function (yargs) {
+      console.log('Adding a new note!');
+    }
+  )
+  .command(
+    'remove',
+    'Remove a note',
+    function (yargs) {
+      console.log('Removing the note');
+    }
+  )
+  .command(
+    'list',
+    'List your notes',
+    function (yargs) {
+      console.log('Listing out all notes');
+    }
+  )
+  .command(
+    'read',
+    'Read a note',
+    function (yargs) {
+      console.log('Reading a note');
+    }
+  )
+  .parse(hideBin(process.argv));
